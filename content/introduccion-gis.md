bd14c78ce0c143c766cda2b8041d70b898a8da42
jlautarosilva <jlautarosilva@gmail.com>
2016-06-08 18:46:12 -0300

    Commit inicial blog Init7

---
+++ content/introduccion-gis.md
@@ -0,0 +1,14 @@
+Title: Introduccion a los GIS con Software Libre
+Date: 2016-06-08 18:20
+Category: GIS
+
+Lista de guias para iniciar en GIS utilizando Software Libre.
+
+* Introduccion a QGIS
+http://docs.qgis.org/2.8/es/docs/gentle_gis_introduction/introducing_gis.html
+
+* Django
+https://docs.djangoproject.com/en/dev/intro/tutorial01/
+
+* Geodjango
+https://docs.djangoproject.com/es/1.9/ref/contrib/gis/tutorial/
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Lautaro Silva'
+SITENAME = u'Init 7 Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Santiago'
+
+DEFAULT_LANG = u'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('TU COLECTIVO', 'http://tucolectivo.cl/'),
+         ('Gore Biobio', 'http://sitio.gorebiobio.cl/'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/init7.cl/'),
+          ('Twitter', 'https://twitter.com/Init7cl'),
+          ('LinkedIn','https://www.linkedin.com/company/10170575'),
+        )
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
